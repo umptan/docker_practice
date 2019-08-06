350a1ce25e550cbbbef0c91b2ee2d81e807d9121
urara <urarara0313@gmail.com>
2019-08-06 14:17:27 +0900

    新しく自画像を追加した

--- html/index.php
+++ html/index.php
@@ -4,9 +4,11 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="/styles/index.css">
     <title>うららのホームページ</title>
 </head>
 <body>
     <header>うららのホームページ</header>
+    <section><img src="/images/profile.png" alt="自画像"></section>
 </body>
 </html>
