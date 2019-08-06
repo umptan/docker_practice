73efba2e883a0c3a612b39419190523d786e2537
urara <urarara0313@gmail.com>
2019-08-06 13:56:59 +0900

    hp_tittle作成

--- html/index.php
+++ html/index.php
@@ -1,14 +1,12 @@
 <!DOCTYPE html>
-<html lang="ja">
+<html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>はじめてのphp</title>
+    <title>うららのホームページ</title>
 </head>
 <body>
-   <h1>
-       はじめてのphp
-   </h1> 
+    <header>うららのホームページ</header>
 </body>
 </html>
