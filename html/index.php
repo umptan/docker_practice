babbc2417347276df65c9d9c4286e3c50bdeda82
urara <urarara0313@gmail.com>
2019-08-06 16:15:11 +0900

    名前にIDをつけた

--- html/index.php
+++ html/index.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="ja">
 
 <head>
     <meta charset="UTF-8">
@@ -13,7 +13,7 @@
     <header>
         <h1>うららのホームページ</h1>
     </header>
-    <section><img src="/images/profile.png" alt="自画像">
+    <section id="profile"><img src="/images/profile.png" alt="自画像">
         <ul>
             <li>浦　英稔　（うら　ひでみ）</li>
             <li>生年月日　1997年12月13日</li>
@@ -21,12 +21,12 @@
             <li><a href="#">プロフィール</a></li>
         </ul>
     </section>
-    <section class="contact-message">
-        <span >If you have any enquiries regarding my TV drama or film, or would like to make an enquiry concerning future projects, please do not hesitate to contact me through the following email address.
+    <section id="contact" class="contact-message">
+        <span>If you have any enquiries regarding my TV drama or film, or would like to make an enquiry concerning future projects, please do not hesitate to contact me through the following email address.
         </span>
         <div>mail: <a href="mailto:urarara0313@gmail.com">urarara0313@gmail.com</a></div>
     </section>
-    <section>
+    <section id="adless">
         <div>所属</div>
         <div>オフィス　エンスペース</div>
         <div>980-9999</div>
@@ -34,20 +34,21 @@
         <div>TEL: 022-123-4567</div>
         <div>FAX: 022-123-4567</div>
     </section>
-    <section><h3>★★★更新情報★★★</h3>
-    <article>
-        <h4>舞台</h4>
-        <div>そろそろ暑くなってきたなぁ</div>
-    </article>
-    <article>
-        <h4>TV</h4>
-        <div>最近面白いテレビないなぁ</div>
-    </article>
-    <article>
-        <h4>映画</h4>
-        <div>映画見ないなぁ</div>
-    </article>
-</section>
+    <section id="history">
+        <h3>★★★更新情報★★★</h3>
+        <article>
+            <h4>舞台</h4>
+            <div>そろそろ暑くなってきたなぁ</div>
+        </article>
+        <article>
+            <h4>TV</h4>
+            <div>最近面白いテレビないなぁ</div>
+        </article>
+        <article>
+            <h4>映画</h4>
+            <div>映画見ないなぁ</div>
+        </article>
+    </section>
 </body>
 
 </html>
