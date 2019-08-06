844683b8366573aa8a81564dc51cd3ad9a740878
urara <urarara0313@gmail.com>
2019-08-06 15:30:30 +0900

    add contact message

--- html/index.php
+++ html/index.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -7,15 +8,24 @@
     <link rel="stylesheet" href="/styles/index.css">
     <title>うららのホームページ</title>
 </head>
+
 <body>
-    <header><h1>うららのホームページ</h1></header>
+    <header>
+        <h1>うららのホームページ</h1>
+    </header>
     <section><img src="/images/profile.png" alt="自画像">
-    <ul>
-        <li>浦　英稔　（うら　ひでみ）</li>
-        <li>生年月日　1997年12月13日</li>
-        <li>血液型　O型</li>
-        <li><a href="#">プロフィール</a></a></li>
-    </ul>
-</section>
+        <ul>
+            <li>浦　英稔　（うら　ひでみ）</li>
+            <li>生年月日　1997年12月13日</li>
+            <li>血液型　O型</li>
+            <li><a href="#">プロフィール</a></li>
+        </ul>
+    </section>
+    <section class="contact-message">
+        <span >If you have any enquiries regarding my TV drama or film, or would like to make an enquiry concerning future projects, please do not hesitate to contact me through the following email address.
+        </span>
+        <div>mail: <a href="mailto:urarara0313@gmail.com">urarara0313@gmail.com</a></div>
+    </section>
 </body>
+
 </html>
