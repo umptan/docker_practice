e17720ff7e9a7950c68712a4393115a24209b150
urara <urarara0313@gmail.com>
2019-08-06 16:33:50 +0900

    不要なCSSの設定を削除した

--- html/index.php
+++ html/index.php
@@ -13,7 +13,7 @@
     <header>
         <h1>うららのホームページ</h1>
     </header>
-    <section id="profile"><img src="/images/profile.png" alt="自画像">
+    <section id="profile"><img src="/images/profile.png" alt="自画像" class="width-200">
         <ul>
             <li>浦　英稔　（うら　ひでみ）</li>
             <li>生年月日　1997年12月13日</li>
@@ -21,7 +21,7 @@
             <li><a href="#">プロフィール</a></li>
         </ul>
     </section>
-    <section id="contact" class="contact-message">
+    <section id="contact" class="width-200">
         <span>If you have any enquiries regarding my TV drama or film, or would like to make an enquiry concerning future projects, please do not hesitate to contact me through the following email address.
         </span>
         <div>mail: <a href="mailto:urarara0313@gmail.com">urarara0313@gmail.com</a></div>
