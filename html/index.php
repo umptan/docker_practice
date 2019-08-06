ca07367323ffa0d1e8ea85f5a9fb725ce3170fe1
urara <urarara0313@gmail.com>
2019-08-06 17:05:03 +0900

    ページを2分割に変更した

--- html/index.php
+++ html/index.php
@@ -13,42 +13,51 @@
     <header>
         <h1>うららのホームページ</h1>
     </header>
-    <section id="profile"><img src="/images/profile.png" alt="自画像" class="width-200">
-        <ul>
-            <li>浦　英稔　（うら　ひでみ）</li>
-            <li>生年月日　1997年12月13日</li>
-            <li>血液型　O型</li>
-            <li><a href="#">プロフィール</a></li>
-        </ul>
-    </section>
-    <section id="contact" class="width-200">
-        <span>If you have any enquiries regarding my TV drama or film, or would like to make an enquiry concerning future projects, please do not hesitate to contact me through the following email address.
-        </span>
-        <div>mail: <a href="mailto:urarara0313@gmail.com">urarara0313@gmail.com</a></div>
-    </section>
-    <section id="adless">
-        <div>所属</div>
-        <div>オフィス　エンスペース</div>
-        <div>980-9999</div>
-        <div>宮城県仙台市青葉区国分町1-1-1</div>
-        <div>TEL: 022-123-4567</div>
-        <div>FAX: 022-123-4567</div>
-    </section>
-    <section id="history">
-        <h3>★★★更新情報★★★</h3>
-        <article>
-            <h4>舞台</h4>
-            <div>そろそろ暑くなってきたなぁ</div>
-        </article>
-        <article>
-            <h4>TV</h4>
-            <div>最近面白いテレビないなぁ</div>
-        </article>
-        <article>
-            <h4>映画</h4>
-            <div>映画見ないなぁ</div>
-        </article>
-    </section>
+    <div id="content">
+        <div id="content-left">
+            <section id="profile"><img src="/images/profile.png" alt="自画像" class="width-200">
+                <ul>
+                    <li>浦　英稔　（うら　ひでみ）</li>
+                    <li>生年月日　1997年12月13日</li>
+                    <li>血液型　O型</li>
+                    <li><a href="#">プロフィール</a></li>
+                </ul>
+            </section>
+            <section id="contact" class="width-200">
+                <span>If you have any enquiries regarding my TV drama or film, or would like to make an enquiry concerning future projects, please do not hesitate to contact me through the following email address.
+                </span>
+                <div>mail: <a href="mailto:urarara0313@gmail.com">urarara0313@gmail.com</a></div>
+            </section>
+            <section id="adless">
+                <div>所属</div>
+                <div>オフィス　エンスペース</div>
+                <div>980-9999</div>
+                <div>宮城県仙台市青葉区国分町1-1-1</div>
+                <div>TEL: 022-123-4567</div>
+                <div>FAX: 022-123-4567</div>
+            </section>
+
+        </div>
+        <div id="content-right">
+            <section id="history">
+                <h3>★★★更新情報★★★</h3>
+                <article>
+                    <h4>舞台</h4>
+                    <div>そろそろ暑くなってきたなぁ</div>
+                </article>
+                <article>
+                    <h4>TV</h4>
+                    <div>最近面白いテレビないなぁ</div>
+                </article>
+                <article>
+                    <h4>映画</h4>
+                    <div>映画見ないなぁ</div>
+                </article>
+            </section>
+
+        </div>
+
+    </div>
 </body>
 
 </html>
